6d7c7490409cee2668057a774b712535c9354098
kondo <kondo@a1dc017f-dc0b-0410-9f91-a1a664c6b1c4>
2012-01-30 02:59:31 +0000

    [SDK]Windows環境ではパスの問題で起動できない問題に対応
    git-svn-id: svn+ssh://dev.change-vision.com/home/svnreps/jude/trunk/maven/astah-maven-plugin@38707 a1dc017f-dc0b-0410-9f91-a1a664c6b1c4

--- src/main/java/com/change_vision/astah/PluginPathsBuilder.java
+++ src/main/java/com/change_vision/astah/PluginPathsBuilder.java
@@ -9,46 +9,46 @@ import java.util.List;
 import org.apache.commons.io.FilenameUtils;
 
 public class PluginPathsBuilder {
-	
-	private static final String SPACE = " ";
-	private ASDK sdk = new ASDK();
-	private List<File> plugins = new ArrayList<File>();
 
-	public PluginPathsBuilder(File target) {
-		if(target == null) throw new IllegalArgumentException("target is null.");
-		plugins.add(target);
-	}
-	
-	PluginPathsBuilder(ASDK sdk,File target){
-		this(target);
-		this.sdk = sdk;
-	}
+  private static final String SPACE = " ";
+  private ASDK sdk = new ASDK();
+  private List<File> plugins = new ArrayList<File>();
 
-	public String build() {
-		addBundles();
-		StringBuilder pluginsBuilder = new StringBuilder();
-		for (File plugin : plugins) {
-			pluginsBuilder.append(createPluginPath(plugin));
-			pluginsBuilder.append(SPACE);
-		}
-		String paths = pluginsBuilder.toString().trim();
-		return format("-Dplugin_list=%s",paths);
-	}
+  public PluginPathsBuilder(File target) {
+    if(target == null) throw new IllegalArgumentException("target is null.");
+    plugins.add(target);
+  }
 
-	private String createPluginPath(File plugin) {
-		 String path = FilenameUtils.separatorsToUnix(plugin.getAbsolutePath());
-		if(plugin.isDirectory()){
-			return format("reference:file://%s/", path);
-		} else {
-			return format("file:///%s", path);
-		}
-	}
-	
-	private void addBundles() {
-		List<File> bundles = sdk.getBundlePlugin();
-		for (File bundle : bundles) {
-			plugins.add(0,bundle);
-		}
-	}
+  PluginPathsBuilder(ASDK sdk,File target){
+    this(target);
+    this.sdk = sdk;
+  }
+
+  public String build() {
+    addBundles();
+    StringBuilder pluginsBuilder = new StringBuilder();
+    for (File plugin : plugins) {
+      pluginsBuilder.append(createPluginPath(plugin));
+      pluginsBuilder.append(SPACE);
+    }
+    String paths = pluginsBuilder.toString().trim();
+    return format("-Dplugin_list=%s",paths);
+  }
+
+  private String createPluginPath(File plugin) {
+     String path = FilenameUtils.separatorsToUnix(plugin.getAbsolutePath());
+    if(plugin.isDirectory()){
+      return format("reference:file:///%s/", path);
+    } else {
+      return format("file:///%s", path);
+    }
+  }
+
+  private void addBundles() {
+    List<File> bundles = sdk.getBundlePlugin();
+    for (File bundle : bundles) {
+      plugins.add(0,bundle);
+    }
+  }
 
 }
