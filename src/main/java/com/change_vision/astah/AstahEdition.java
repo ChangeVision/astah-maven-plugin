d069da73a6e8a6943d69cd1d90913e5c5cc6a4da
y-zhou <yi.zhou@change-vision.com>
2013-10-08 16:44:24 +0900

    support for SysML Edition

--- src/main/java/com/change_vision/astah/AstahEdition.java
+++ src/main/java/com/change_vision/astah/AstahEdition.java
@@ -1,19 +1,18 @@
 package com.change_vision.astah;
 
 public enum AstahEdition {
-	professional,community,uml;
-//		,think;
+	professional,community,uml,sysml;
 	
 	public String getJARName(){
 		switch(this){
+		case sysml :
+			return "astah-sys.jar";
 		case professional :
 			return "astah-pro.jar";
 		case uml :
 			return "astah-uml.jar";
 		case community : 
 			return "astah-community.jar";
-//		case think :
-//			return "astah-think.jar";
 		default:
 			return null;
 		}
